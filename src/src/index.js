165865bd3a85f3aad6b23f9356de056880f7b049
Cody Sheridan <cody.sheridan@protonmail.com>
2024-10-05 15:28:42 -0400

    v1 complete

--- src/src/index.js
+++ src/src/index.js
@@ -1,70 +1,25 @@
-require('./styles.scss');
+import "./styles/main.scss";
+import "./styles/projects.scss";
 
-var Flickity = require('flickity');
-require('flickity-imagesloaded');
-
-var $carousels = new Array();
-
-// Modals
-
-var rootEl = document.documentElement;
-var $modals = getAll('.modal');
-var $modalTriggers = getAll('.modal-trigger');
-var $modalCloses = getAll('.modal-card-head .delete, .modal-card-foot .button');
-
-if ($modalTriggers.length > 0) {
-    $modalTriggers.forEach(function ($el) {
-        $el.addEventListener('click', function () {
-            var target = $el.dataset.target;
-            openModal(target);
+document.addEventListener("DOMContentLoaded", () => {
+  if (
+    window.location.pathname.endsWith("index.html") ||
+    window.location.pathname === "/"
+  ) {
+    const readMoreLink = document.getElementById("read-more");
+    if (readMoreLink) {
+      readMoreLink.addEventListener("click", function () {
+        var contents = document.querySelectorAll(".toggle-content");
+        contents.forEach(function (content) {
+          if (content.classList.contains("show")) {
+            content.classList.remove("show");
+            readMoreLink.textContent = "Read more...";
+          } else {
+            content.classList.add("show");
+            readMoreLink.textContent = "Less...";
+          }
         });
-    });
-}
-
-if ($modalCloses.length > 0) {
-    $modalCloses.forEach(function ($el) {
-        $el.addEventListener('click', function () {
-            closeModals();
-        });
-    });
-}
-
-function openModal(target) {
-    var $target = document.getElementById(target);
-    rootEl.classList.add('is-clipped');
-    $target.classList.add('is-active');
-    var carouselId = target + '-carousel';
-
-    if (document.querySelector('#' + carouselId)) {
-        // Initialize each carousel one time only
-        if ($carousels.length === 0) {
-            $carousels.push(initCarousel(carouselId));
-        }
-        else {
-            var index = $carousels.findIndex(c => c.element.id == carouselId);
-            if (index === -1) {
-                $carousels.push(initCarousel(carouselId));
-            }
-        }
+      });
     }
-}
-
-function closeModals() {
-    rootEl.classList.remove('is-clipped');
-    $modals.forEach(function ($el) {
-        $el.classList.remove('is-active');
-    });
-}
-
-// Functions
-
-function initCarousel(id) {
-    return new Flickity('#' + id, {
-        imagesLoaded: true,
-        adaptiveHeight: true // https://github.com/metafizzy/flickity/issues/11
-    });
-}
-
-function getAll(selector) {
-    return Array.prototype.slice.call(document.querySelectorAll(selector), 0);
-}
+  }
+});
--- src/webpack.config.js
+++ src/webpack.config.js
@@ -1,45 +1,121 @@
-const path = require('path');
-const TerserJSPlugin = require('terser-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
+// module.exports = {
+//   entry: "./src/index.js", // Correct path for entry point
+//   watch: true,
+//   output: {
+//     path: path.resolve(__dirname, "dist"),
+//     filename: "js/[name].[contenthash].bundle.js", // Include content hash for cache busting
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /\.s?css$/,
+//         use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+//       },
+//       {
+//         test: /\.html$/,
+//         use: [
+//           {
+//             loader: "html-loader",
+//             options: { minimize: true },
+//           },
+//         ],
+//       },
+//       {
+//         test: /\.js$/,
+//         exclude: /node_modules/,
+//         use: {
+//           loader: "babel-loader",
+//           options: {
+//             presets: ["@babel/preset-env"],
+//           },
+//         },
+//       },
+//     ],
+//   },
+//   plugins: [
+//     new HtmlWebpackPlugin({
+//       template: "./index.html", // Correct path to index.html
+//       filename: "index.html",
+//       chunks: ["main"], // Include main JavaScript bundle
+//     }),
+//     // new HtmlWebpackPlugin({
+//     //   template: "projects/toolio.html",
+//     //   filename: "toolio.html",
+//     // }),
+//     new MiniCssExtractPlugin({
+//       filename: "css/[name].[contenthash].bundle.css", // Include content hash for cache busting
+//     }),
+//     new CopyWebpackPlugin({
+//       patterns: [
+//         { from: "docs/resume.pdf", to: "docs/resume.pdf" }, // Copy resume.pdf to dist/docs
+//       ],
+//     }),
+//   ],
+//   optimization: {
+//     minimizer: [new TerserJSPlugin({}), new CssMinimizerPlugin()],
+//     runtimeChunk: false, // Avoid extra runtime chunk file
+//     splitChunks: {
+//       cacheGroups: {
+//         default: false, // Disable Webpack from creating additional chunks
+//       },
+//     },
+//   },
+// };
 
 module.exports = {
-  entry: './src/index.js',
-  watch: true,
+  entry: {
+    bundle: path.resolve(__dirname, "src/index.js"),
+  },
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'js/bundle.js'
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name][contenthash].js",
+    clean: true,
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
   },
   module: {
-    rules: [{
-      test: /.s?css$/,
-      use: [
-          MiniCssExtractPlugin.loader,
-          {
-            loader: 'css-loader'
-          },
-          {
-            loader: 'sass-loader',
-            options: {
-              sourceMap: true,
-              // options...
-            }
-          }
-        ]
-    }]
-  },
-  optimization: {
-    minimizer: [
-      // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
-      // `...`,
-      new CssMinimizerPlugin(),
-      new TerserJSPlugin({})
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
     ],
   },
+  resolve: {
+    modules: [path.resolve(__dirname, "node_modules"), "node_modules"],
+  },
   plugins: [
-    new MiniCssExtractPlugin({
-      filename: 'css/[name].bundle.css'
+    new HtmlWebpackPlugin({
+      title: "Cody Sheridan",
+      filename: "index.html",
+      template: "src/index.html",
     }),
-  ]
+    new HtmlWebpackPlugin({
+      title: "Cody Sheridan | Toolio",
+      filename: "projects/toolio.html",
+      template: "src/projects/toolio.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "styles/[name].css",
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: path.resolve(__dirname, "src/docs"), to: "docs" },
+        { from: path.resolve(__dirname, "src/img"), to: "img" },
+      ],
+    }),
+  ],
 };
