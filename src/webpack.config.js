30cb75e63797afc97fae1156343ebfc95dec32d9
Cody Sheridan <cody.sheridan@protonmail.com>
2024-10-05 20:47:51 -0400

    restructured repository

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -3,82 +3,18 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 
-// module.exports = {
-//   entry: "./src/index.js", // Correct path for entry point
-//   watch: true,
-//   output: {
-//     path: path.resolve(__dirname, "dist"),
-//     filename: "js/[name].[contenthash].bundle.js", // Include content hash for cache busting
-//   },
-//   module: {
-//     rules: [
-//       {
-//         test: /\.s?css$/,
-//         use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
-//       },
-//       {
-//         test: /\.html$/,
-//         use: [
-//           {
-//             loader: "html-loader",
-//             options: { minimize: true },
-//           },
-//         ],
-//       },
-//       {
-//         test: /\.js$/,
-//         exclude: /node_modules/,
-//         use: {
-//           loader: "babel-loader",
-//           options: {
-//             presets: ["@babel/preset-env"],
-//           },
-//         },
-//       },
-//     ],
-//   },
-//   plugins: [
-//     new HtmlWebpackPlugin({
-//       template: "./index.html", // Correct path to index.html
-//       filename: "index.html",
-//       chunks: ["main"], // Include main JavaScript bundle
-//     }),
-//     // new HtmlWebpackPlugin({
-//     //   template: "projects/toolio.html",
-//     //   filename: "toolio.html",
-//     // }),
-//     new MiniCssExtractPlugin({
-//       filename: "css/[name].[contenthash].bundle.css", // Include content hash for cache busting
-//     }),
-//     new CopyWebpackPlugin({
-//       patterns: [
-//         { from: "docs/resume.pdf", to: "docs/resume.pdf" }, // Copy resume.pdf to dist/docs
-//       ],
-//     }),
-//   ],
-//   optimization: {
-//     minimizer: [new TerserJSPlugin({}), new CssMinimizerPlugin()],
-//     runtimeChunk: false, // Avoid extra runtime chunk file
-//     splitChunks: {
-//       cacheGroups: {
-//         default: false, // Disable Webpack from creating additional chunks
-//       },
-//     },
-//   },
-// };
-
 module.exports = {
   entry: {
-    bundle: path.resolve(__dirname, "src/index.js"),
+    bundle: path.resolve(__dirname, "index.js"),
   },
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "../dist"),
     filename: "[name][contenthash].js",
     clean: true,
   },
   devServer: {
     static: {
-      directory: path.resolve(__dirname, "dist"),
+      directory: path.resolve(__dirname, "../dist"),
     },
     port: 3000,
     open: true,
@@ -101,20 +37,20 @@ module.exports = {
     new HtmlWebpackPlugin({
       title: "Cody Sheridan",
       filename: "index.html",
-      template: "src/index.html",
+      template: "index.html",
     }),
     new HtmlWebpackPlugin({
       title: "Cody Sheridan | Toolio",
       filename: "projects/toolio.html",
-      template: "src/projects/toolio.html",
+      template: "projects/toolio.html",
     }),
     new MiniCssExtractPlugin({
       filename: "styles/[name].css",
     }),
     new CopyWebpackPlugin({
       patterns: [
-        { from: path.resolve(__dirname, "src/docs"), to: "docs" },
-        { from: path.resolve(__dirname, "src/img"), to: "img" },
+        { from: path.resolve(__dirname, "docs"), to: "docs" },
+        { from: path.resolve(__dirname, "img"), to: "img" },
       ],
     }),
   ],
